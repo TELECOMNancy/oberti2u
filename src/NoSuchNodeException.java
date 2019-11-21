3f6b23f7f93466150efad24b0fba9d0120a16034
LATA Warren <warren.lata@telecomnancy.eu>
2019-11-21 15:46:32 +0100

    Upload New File

---
+++ src/NoSuchNodeException.java
@@ -0,0 +1,16 @@
+/*
+ * Exception pour le compilateur : pas de tel noeud.
+ * @author : Lata Warren - Rehioui Walid - Khay Oussama - Lallement Eliott
+ * tiger - PCL 2018 - TELECOM Nancy
+ */
+
+public class NoSuchNodeException extends Exception {
+
+  public NoSuchNodeException() {
+    super();
+  }
+
+  public NoSuchNodeException(String s) {
+    super(s);
+  }
+}
